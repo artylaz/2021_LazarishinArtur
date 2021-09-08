ef2c6e3f32bb9919e2410f9710a6f21fc73be3a2
Владислав Лавров <55340643+VladislavLavrov@users.noreply.github.com>
2021-09-08 12:37:34 +0500

    Версия 1.21.09.01. Начало

---
+++ 2021_LazarishinArtur/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _2021_LazarishinArtur.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
